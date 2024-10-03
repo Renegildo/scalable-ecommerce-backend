94e57b9c688aa786a024654e98df791559f313e2
Renegildo <renegildo@proton.me>
2024-10-03 12:22:04 -0300

    feat: add payment-service stripe integration

--- payment-service/src/controllers/payment.ts
+++ payment-service/src/controllers/payment.ts
@@ -1,17 +1,26 @@
 import express from "express";
 import pool from "../db";
+import stripe from "../lib/stripe";
 
 export const createPayment = async (req: express.Request, res: express.Response) => {
   try {
-    const { order_id, amount, status, payment_method } = req.body;
+    const { order_id, amount, payment_method_id, currency } = req.body;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      payment_method: payment_method_id,
+      return_url: process.env.STRIPE_RETURN_URL,
+      amount: amount * 100,
+      confirm: true,
+      currency,
+    });
 
     await pool.query(
       `INSERT INTO payment (order_id, amount, status, payment_method)
       VALUES ($1, $2, $3, $4)`,
-      [order_id, amount, status, payment_method]
+      [order_id, amount, paymentIntent.status, paymentIntent.payment_method]
     );
 
-    res.status(200).json({msg: "Payment created successfully."});
+    res.status(200).json(paymentIntent);
   } catch (error) {
     console.log(error);
     res.sendStatus(400);
